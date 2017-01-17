d67832fe8c259460814707c8ce2f9abba67907c7
Thomas Bodine <bo_dance@yahoo.com>
2017-01-17 16:13:56 -0600

    Bodine needs to implement exception, for new features

---
+++ src/java/com/tommythegeek/sackLunch/dao/BodineNeedsToImplement.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2016 Thomas Bodine
+ * All rights Reserved
+ */
+package com.tommythegeek.sackLunch.dao;
+
+/**
+ *
+ * @author Owner
+ */
+public class BodineNeedsToImplement extends Exception {
+
+    /**
+     * Creates a new instance of <code>BodineNeedsToImplement</code> without
+     * detail message.
+     */
+    public BodineNeedsToImplement() {
+    }
+
+    /**
+     * Constructs an instance of <code>BodineNeedsToImplement</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public BodineNeedsToImplement(String msg) {
+        super(msg);
+    }
+}
