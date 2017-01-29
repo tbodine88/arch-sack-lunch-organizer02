0d1aa54d13c7bd44259cda48f7af84b941689d1d
Thomas Bodine <bo_dance@yahoo.com>
2017-01-29 07:22:38 -0600

    authorship added to sendemail

--- bash/email/sendemail.sh
+++ bash/email/sendemail.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 # A simple script to send email from Thomas Bodine's email account on
 # Cactus
+# Author Thomas Bodine
 if [ -e message ] ; then
   cat message.txt | ssh tbodine@outserv.cactus.org mail -s archSackLunchMessage tom@tommythegeek.com
   rm message
